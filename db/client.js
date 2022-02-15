cdfe38593563d7b4ab1b419a93a112d4b8a5a9d8
Reuben Ramos <rsramos23@gmail.com>
2022-02-15 01:41:42 -0800

    client.js completed

--- db/client.js
+++ db/client.js
@@ -1,2 +1,4 @@
 // build and export your unconnected client here
-
+const { Client } = require('pg');
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/fitness-dev');
+module.exports = client;
