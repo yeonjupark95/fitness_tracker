54067dc2e2187a532b2a5c2dd283aae9df375685
Reuben Ramos <rsramos23@gmail.com>
2022-02-15 20:51:28 -0800

    completed DB index.js and added DB js files

---
+++ db/users.js
@@ -0,0 +1,21 @@
+const {client} = require('./client')
+
+async function createUser({ username, password }) {
+    try {
+      const {rows:[user]} = await client.query(`
+      INSERT INTO users(username, password) 
+      VALUES ($1, $2)
+      ON CONFLICT (username) DO NOTHING
+      RETURNING *;
+`, [ username, password ]);
+
+    delete user.password
+      return user
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  module.exports = {
+    createUser,
+  }
