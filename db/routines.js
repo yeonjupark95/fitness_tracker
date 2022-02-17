9add1b671e36dca85288a98efa9d35ca274c7062
Reuben Ramos <rsramos23@gmail.com>
2022-02-17 10:52:25 -0800

    implemented getRoutinesWithoutActivites function

--- db/routines.js
+++ db/routines.js
@@ -16,7 +16,35 @@ async function getRoutineById(id) {
     }
   }
 
+  async function getRoutinesWithoutActivities() {
+    try{
+    const { rows: routines } = await client.query(
+        `SELECT *
+        FROM routines;
+      `);
+    
+      return routines;
+    }catch (error){
+      throw error;
+    }
+  }
+
+  async function getAllRoutines() {
+    try{
+    const { rows: [routine] } = await client.query(
+        `SELECT *
+        FROM routines;
+      `);
+    
+      return routine;
+    }catch (error){
+      throw error;
+    }
+  }
+
 
 module.exports = {
     getRoutineById,
+    getRoutinesWithoutActivities,
+    getAllRoutines,
   };
