f4e225d29b16b639f15352d824850b3c3ae0bd62
Yeonju Park <yeonjupark95@gmail.com>
2022-02-16 23:04:37 -0800

    added updateActivity to activities.js

--- db/activities.js
+++ db/activities.js
@@ -30,29 +30,46 @@ async function getAllAcitivities() {
   }
 }
 
-async function createActivity({ name, description}) {
-    try {
-      const {
-        rows: [activity],
-      } = await client.query(
-        `
+async function createActivity({ name, description }) {
+  try {
+    const {
+      rows: [activity],
+    } = await client.query(
+      `
           INSERT INTO activities(name, description) 
           VALUES ($1, $2)
           ON CONFLICT (name) DO NOTHING
           RETURNING *;
         `,
-        [name, description]
-      );
-  
-      return activity;
-    } catch (error) {
-      throw error;
-    }
-  }
+      [name, description]
+    );
 
+    return activity;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateActivity({ id, name, description }) {
+  try {
+    const {
+      row: [activity],
+    } = client.query(
+      `
+      UPDATE activities
+      WHERE id = $1, $2;
+    `,
+      [name, description]
+    );
+    return activity;
+  } catch (error) {
+    throw error;
+  }
+}
 
 module.exports = {
-   getActivityById,
-   getAllAcitivities,
-   createActivity,
-  };
+  getActivityById,
+  getAllAcitivities,
+  createActivity,
+  updateActivity
+};
