07e0370ed842d72dd0ef89cdf0e42fbd5e211422
Yeonju Park <yeonjupark95@gmail.com>
2022-02-16 18:55:33 -0800

    added hash for getUser

--- db/users.js
+++ db/users.js
@@ -2,12 +2,31 @@ const { client } = require("./client");
 
 async function getUser({ username, password }) {
   try {
-    const {rows: [user]} = await client.query(`
+    const {
+      rows: [user],
+    } = await client.query(
+      `
       SELECT * FROM users
       WHERE username = $1;
-    `, [username]);
-    return user;
+    `,
+      [username]
+    );
+
+    if (!user) {
+      throw {
+        name: "userNotFound",
+        message: "User not found under that username",
+      };
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
+      delete user.password;
+      return user;
+    }
   } catch (error) {
+    console.error(error);
     throw error;
   }
 }
