f533356c32909c1e0b5962ad0afed4ae48287a46
Yeonju Park <yeonjupark95@gmail.com>
2022-02-16 22:37:20 -0800

    added getAllActitivies to activities.js

--- db/activities.js
+++ db/activities.js
@@ -1,20 +1,36 @@
-const  client  = require("./client");
+const client = require("./client");
 
 async function getActivityById(id) {
-    try{
-      const {rows: [activity]} = await client.query(`
+  try {
+    const {
+      rows: [activity],
+    } = await client.query(
+      `
         SELECT *
         FROM activities
         WHERE id=$1;
-      `,[id]);
+      `,
+      [id]
+    );
 
-      return activity;
-    } catch (error){
-      throw error;
-    }
+    return activity;
+  } catch (error) {
+    throw error;
   }
+}
 
+async function getAllAcitivities() {
+  try {
+    const { rows: activities } = await client.query(`
+      SELECT * FROM activities    
+    `);
+    return activities;
+  } catch (error) {
+    throw error;
+  }
+}
 
 module.exports = {
-   getActivityById,
-  };
+  getActivityById,
+  getAllAcitivities,
+};
