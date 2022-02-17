171e5ae36f73dd048afb385726971a9de04f0531
Reuben Ramos <rsramos23@gmail.com>
2022-02-16 19:12:22 -0800

    Added hash to createUser

--- db/users.js
+++ db/users.js
@@ -1,13 +1,14 @@
 const {client} = require('./client')
 
 async function createUser({ username, password }) {
+    const hashPassword = await bcrypt.hash(password, SALT_ROUNDS)
     try {
       const {rows:[user]} = await client.query(`
       INSERT INTO users(username, password) 
       VALUES ($1, $2)
       ON CONFLICT (username) DO NOTHING
       RETURNING *;
-`, [ username, password ]);
+`, [ username, hashPassword ]);
 
     delete user.password
       return user
