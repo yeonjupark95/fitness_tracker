b5554c474192ff6b731900d379165f20ba77bed2
Reuben Ramos <rsramos23@gmail.com>
2022-02-19 09:16:04 -0800

    implemented getRoutineActivityById function

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -21,4 +21,20 @@ async function addActivityToRoutine({
   }
 }
 
-module.exports = { addActivityToRoutine };
+async function getRoutineActivityById(id){
+  try{
+        const {rows:[routine_activity]} = await client.query(
+    `
+    SELECT *
+    FROM routine_activites
+    WHERE id=$1;
+  `,[id]);
+    return routine_activity;
+  } catch (error) {
+    throw(error)
+  }
+}
+module.exports = { 
+    addActivityToRoutine,
+    getRoutineActivityById,
+ };
