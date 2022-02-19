5bc6b19d5239e5d998dfd41dadd0babbbfbdb30d
Reuben Ramos <rsramos23@gmail.com>
2022-02-18 19:19:16 -0800

    added activities to getAllRoutines

--- db/routines.js
+++ db/routines.js
@@ -15,8 +15,26 @@ async function getRoutineById(id) {
       throw error;
     }
   }
-
-
+  async function getAllRoutines() {
+    try {
+        const { rows: routines} = await client.query(`
+        SELECT routines.*, users.username AS "creatorName"
+        FROM routines
+        JOIN users ON routines."creatorId"=users.id
+    `);
+  
+        const { rows: activities } = await client.query(`
+        SELECT activities.*, routine_activities.duration, routine_activities.count, routine_activities.id AS "routineActivityId", routine_activities."routineId"
+        FROM activities
+        JOIN routine_activities ON routine_activities."activityId" = activities.id
+    `);
+    
+        return routines;
+    }   catch (error){
+        throw error;
+    }
+  }
 module.exports = {
     getRoutineById,
+    getAllRoutines,
   };
