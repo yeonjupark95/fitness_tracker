f469b751e5d05bb0a1eed52d8cdb4eedeff83aa5
Yeonju Park <yeonjupark95@gmail.com>
2022-02-19 20:45:45 -0800

    added healthRouter to index

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const apiRouter = express.Router();
 
+const healthRouter = require("./health");
+apiRouter.use("/health", healthRouter);
+
 const usersRouter = require("./users");
 apiRouter.use("/users", usersRouter);
 
@@ -12,5 +15,12 @@ apiRouter.use("/activities", activitiesRouter);
 
 const routinesActivitiesRouter = require("./routines_activities");
 apiRouter.use("/routinesActivities", routinesActivitiesRouter);
+
+apiRouter.use((error, req, res, next)=>{
+    res.send({
+        name: error.name,
+        message: error.message,
+    })
+})
 
 module.exports = apiRouter;
