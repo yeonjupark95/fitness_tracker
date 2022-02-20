71d5cab66f111ee1c8737e94734dddd3025a133e
Yeonju Park <yeonjupark95@gmail.com>
2022-02-19 16:45:26 -0800

    created /register in users

--- api/users.js
+++ api/users.js
@@ -2,10 +2,37 @@
 const express = require("express");
 const usersRouter = express.Router();
 const jwt = require("jsonwebtoken");
+const { createUser, getUserByUsername } = require("../db/users");
 // POST /users/register
 // Create a new user. Require username and password, and hash password before saving user to DB. Require all passwords to be at least 8 characters long.
-
 // Throw errors for duplicate username, or password-too-short.
+usersRouter.post("/register", async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const user = await createUser(username, password);
+    if (password.length < 8) {
+      next({
+        name: "password-too-short",
+        message: "Your password is too short!",
+      });
+      return;
+    }
+    const duplicateUsername = await getUserByUsername(username);
+    if (duplicateUsername) {
+      next({
+        name: "dupicate-username",
+        message: "That username already exists!",
+      });
+      return;
+    }
+    res.send({
+      message: "thank you for registering",
+      user,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 // POST /users/login
 // Log in the user. Require username and password, and verify that plaintext login password matches the saved hashed password before returning a JSON Web Token.
