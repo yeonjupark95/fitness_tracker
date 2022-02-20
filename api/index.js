d00b029b70a6526558febd70c5560ca3c15e11f6
Yeonju Park <yeonjupark95@gmail.com>
2022-02-20 13:49:28 -0800

    added health back to main index after a merge error

--- api/index.js
+++ api/index.js
@@ -1,43 +1,51 @@
 const express = require("express");
 const apiRouter = express.Router();
-const {getUserById} = require('../db/users')
-const jwt = require('jsonwebtoken')
-const {JWT_SECRET} = process.env
+const { getUserById } = require("../db/users");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
 
-apiRouter.use(async(req, res, next) => {
-    const prefix = 'Bearer ';
-    const auth = req.header('Authorization');
-    if (!auth){
-        next();
-    } else if (auth.startsWith(prefix)){
+apiRouter.get("/health", async (req, res, next) => {
+  try {
+    res.send({
+      message: "All is well",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
+apiRouter.use(async (req, res, next) => {
+  const prefix = "Bearer ";
+  const auth = req.header("Authorization");
+  if (!auth) {
+    next();
+  } else if (auth.startsWith(prefix)) {
     // const [ , token] = auth.split(' ')
     const token = auth.slice(prefix.length);
-        try{ 
-            const {id} = jwt.verify(token, JWT_SECRET);
-            if (id) {
-            req.user = await getUserById(id);
-            next();
-    //   console.log("User is set:", req.user);
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+      if (id) {
+        req.user = await getUserById(id);
+        next();
+        //   console.log("User is set:", req.user);
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
     }
-    }catch ({name, message}){
-    next({name, message});
-    }
-    }else{
+  } else {
     next({
-        name: 'AuthorizationHeaderError',
-        message: `Authorization token must start with ${ prefix }`
-  });
-}
+      name: "AuthorizationHeaderError",
+      message: `Authorization token must start with ${prefix}`,
+    });
+  }
 });
 
 apiRouter.use((req, res, next) => {
-    if (req.user) {
-      console.log("User is set:", req.user);
-    }
-  
-    next();
-  });
+  if (req.user) {
+    console.log("User is set:", req.user);
+  }
+  next();
+});
 
 const usersRouter = require("./users");
 apiRouter.use("/users", usersRouter);
