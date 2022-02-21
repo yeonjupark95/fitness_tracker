1e40bf218916800c72338229943bf4bf11bd1adf
Yeonju Park <yeonjupark95@gmail.com>
2022-02-20 17:03:35 -0800

    created activitiesRouter.post

--- api/activities.js
+++ api/activities.js
@@ -1,21 +1,32 @@
 // activities
 const express = require("express");
 const activitiesRouter = express.Router();
-const {getAllActivities} = require('../db')
+const { requireUser } = require("./utils");
+const { getAllActivities, createActivity } = require("../db");
 
 // GET /activities
 // Just return a list of all activities in the database
 activitiesRouter.get("/", async (req, res, next) => {
-    try {
-      const activities = await getAllActivities();
-  
-      res.send(activities);
-    } catch (error) {
-      next(error);
-    }
-  });
+  try {
+    const activities = await getAllActivities();
+
+    res.send(activities);
+  } catch (error) {
+    next(error);
+  }
+});
+
 // POST /activities (*)
 // Create a new activity
+activitiesRouter.post("/", requireUser, async (req, res, next) => {
+  const { name, description } = req.body;
+  try {
+    const newActivity = await createActivity({ name, description });
+    res.send(newActivity);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // PATCH /activities/:activityId (*)
 // Anyone can update an activity (yes, this could lead to long term problems a la wikipedia)
