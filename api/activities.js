830166ee3f3dfa76bc9ae32225559ba8e5e5e824
Reuben Ramos <rsramos23@gmail.com>
2022-02-20 18:41:03 -0800

    implemented routines/GET/Post-API

--- api/activities.js
+++ api/activities.js
@@ -1,7 +1,7 @@
 // activities
 const express = require("express");
 const activitiesRouter = express.Router();
-const {getAllActivities} = require('../db')
+const {getAllActivities, createActivity} = require('../db')
 
 // GET /activities
 // Just return a list of all activities in the database
@@ -16,7 +16,15 @@ activitiesRouter.get("/", async (req, res, next) => {
   });
 // POST /activities (*)
 // Create a new activity
-
+activitiesRouter.post('/', async(req, res, next) =>{
+    const { name, description } = req.body;
+    try{
+    const newActivity = await createActivity(req.body)
+    res.send(newActivity)
+    }catch(error){
+     next(error)   
+    }
+    });
 // PATCH /activities/:activityId (*)
 // Anyone can update an activity (yes, this could lead to long term problems a la wikipedia)
 
